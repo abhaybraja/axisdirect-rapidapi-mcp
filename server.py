436fda00d9890126e2e125d2afa3e8facaea05b3
Abhay Braja <72004597+abhaybraja@users.noreply.github.com>
2025-06-06 23:50:25 +0530

    server.py bugs

--- server.py
+++ server.py
@@ -3,6 +3,14 @@ from mcp.server.fastmcp import FastMCP
 # Create an MCP server
 mcp = FastMCP("AxisDirect RAPID API")
 
-@mcp.tool
-def get_profile():
-    pass
+
+@mcp.resource("config://app")
+def get_config() -> str:
+    """Static configuration data"""
+    return "App configuration here"
+
+
+@mcp.resource("users://{user_id}/profile")
+def get_user_profile(user_id: str) -> str:
+    """Dynamic user data"""
+    return f"Profile data for user {user_id}"
